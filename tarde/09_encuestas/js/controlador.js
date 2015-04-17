efa98464e1a6f74687bd92dc7f0bbb36403d89cb
Vicente <vinccento@gmail.com>
2015-04-17 18:37:29 +0200

    commit

--- tarde/09_encuestas/js/controlador.js
+++ tarde/09_encuestas/js/controlador.js
@@ -4,73 +4,70 @@ var controlador =  {
     
     pantallaInicial : $('#landingPage'),
     pantallaPregunta : $('#preguntaPage'),
-	pantallaResultados : $('#resultadosPage'),
     cajaPregunta : $('#preguntaPage h1'),
-	
     
-	inicializar : function() {
-        console.log('Inicializando sistema.', this);
-        this.inicializarUI();
-		this.inicializarNavegacion();
-    },
-	
     inicializarUI : function() {
         var self = this;
         console.log('Inicializando la UI.', this);
         this.botonCargar.on('click', function() {
             console.log('Pulsado cargar.' , this);
-            self.mostrarPregunta(); 
-        });        
+            self.mostrarPregunta();
+        });
+        
     },
-	
-	inicializarNavegacion : function(){
-		var self = this;
-		History.Adapter.bind(window,'statechange',function(){
-			self.procesarStateChange();
-    	});
-	},
-	
-	mostrarPregunta : function(){
-		var self = this;
-		var promesa = modelo.obtenerPregunta('hoy');
-		promesa.done(function(datos){
-			self.cajaPregunta.text(datos.texto);
-			self.navegar(self.pantallaPregunta);
-		});
-	},
     
-	navegar : function(pantallaDestino){
-		var id = pantallaDestino.attr('id');
-		var url = id + '.html';
-		var title = pantallaDestino.attr('data-titulo');
-		var info = {
-				pantallaId : id,
-				hora : new Date()
-		};
-		
-		//manipular el historial del navegador
-		//para poder navegar por nuestra aplicacion
-		//de una single page application.
-		History.pushState(info, title, url);
-	},    
+    mostrarPregunta :  function() {
+        var self = this;
+        var promesa = modelo.obtenerPregunta('hoy');
+        promesa.done(function(datos) {
+            self.cajaPregunta.text(datos.texto);
+            self.navegar(self.pantallaPregunta);
+        });
+    },
     
-	procesarStateChange : function(){		
-		var ruta = location.pathname + location.hash;
-		var posFinal = ruta.lastIndexOf('.html');
-		var posInicial = ruta.indexOf('#');
-		if(posInicial === -1){
-			posInicial = ruta.lastIndexOf('/') + 1;
-		}
-		var id = ruta.substring(posInicial, posFinal);
-		var pantallaDestino = $('#' + id);
-		if (pantallaDestino.length === 0){
-			pantallaDestino = this.pantallaInicial;
-		}
-		this.realizarTransicion(pantallaDestino);
-		
-	},
-	
-	realizarTransicion : function(pantallaDestino) {
+    navegar : function(pantallaDestino) {
+        var id = pantallaDestino.attr('id');
+        var url = id + '.html';
+        var title = pantallaDestino.attr('data-titulo');
+        var info = {
+            pantallaId : id,
+            hora : new Date()
+        };
+        
+        History.pushState(info, title, url);        
+    },
+    
+    obtenerIdPantallaActualPorInfo : function() {
+        var estado = History.getState();
+        var info = estado.data;
+        var id = info.pantallaId;
+        return id;
+    },
+    
+    obtenerIdPantallaActualPorUrl : function(){
+        var ruta = location.pathname + location.hash;
+        var posInicial = ruta.lastIndexOf('/') + 1;
+        var posFinal = ruta.lastIndexOf('.html');
+        var id = null;
+        if (posFinal != -1){
+            id = ruta.substring(posInicial, posFinal);
+        }
+        return id;
+    },
+
+    procesarStateChange : function() {
+        var id = this.obtenerIdPantallaActualPorInfo();
+        if (id === undefined) {
+            id = this.obtenerIdPantallaActualPorUrl();
+        }
+        var pantallaDestino = $('#' + id);
+        if (pantallaDestino.length === 0) {
+            pantallaDestino = this.pantallaInicial;
+        }
+        this.realizarTransicion(pantallaDestino);
+    },
+    
+    realizarTransicion : function(pantallaDestino) {
         $('.activa').fadeOut(function() {
             pantallaDestino.fadeIn(function() {
                 $('.activa').removeClass('activa');
@@ -78,8 +75,24 @@ var controlador =  {
             });
         });
         
-    }  
-};//Fin var Controlador
+    },
+    
+    inicializarNavegacion : function() {  
+        var self = this;
+        History.Adapter.bind(window, 'statechange',
+        function(){ 
+            self.procesarStateChange();
+        });
+    },
+    
+    inicializar : function() {
+        console.log('Inicializando sistema.', this);
+        this.inicializarUI();
+        this.inicializarNavegacion();
+    }
+    
+    
+};
 
 
 $(document).on('ready', function() {
